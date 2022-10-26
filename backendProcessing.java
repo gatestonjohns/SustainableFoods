0a061b8fe59520d8815d370406378cf7625e7d9f
gatestonjohns <gateston136@gmail.com>
2022-10-26 16:35:30 -0400

    more structure

--- backendProcessing.java
+++ backendProcessing.java
@@ -1,19 +1,15 @@
-import java.io.FileNotFoundException;
-
 import dataObjs.ecoPriorityObj;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
 import dataObjs.foodObj;
 import dataObjs.macroPriorityObj;
 
 public class backendProcessing {
     private List<foodObj> totalFoodList;
 
-    public backendProcessing(String csvFilename) throws FileNotFoundException {
-        // here is where we need to read in the CSV and populate the dataObjs.foodObj[] list
+    public backendProcessing(String csvFilename) {
+        // copy in the stuff from eco foods.
     }
 
     public void getIdealFoods(int moneyPriority, ecoPriorityObj ecoPs, macroPriorityObj macPs) {
@@ -22,7 +18,7 @@ public class backendProcessing {
         //WILL NOT BE VOID -- CHANGE RETURN TYPE!!!
     }
 
-    public void gradeWithSuggestions(List<String> inputFoods) {
+    public String gradeWithSuggestions(List<String> inputFoods) {
         // initialize list of foods that user input to empty
         List<foodObj> usersFoods = new ArrayList<>();
 
@@ -36,11 +32,12 @@ public class backendProcessing {
             }
         }
 
+        // we now have all food objects that user eats, so lets grade and find a suggestion!
+        String gradeWithSuggestion = "";
 
 
 
-        // here is where we do grading and return the grade and a critique of the diet
 
-        // WILL NOT BE VOID -- CHANGE RETURN TYPE!!!
+        return gradeWithSuggestion;
     }
 }
